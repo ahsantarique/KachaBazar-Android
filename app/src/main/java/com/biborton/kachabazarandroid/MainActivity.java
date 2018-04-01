dc5f53547bd17aa995d683d0291e0d8eb44b481b
Ahsan <ahsantarique@ymail.com>
2018-04-01 22:35:07 +0600

    page navigation + history navigation

--- app/src/main/java/com/biborton/kachabazarandroid/MainActivity.java
+++ app/src/main/java/com/biborton/kachabazarandroid/MainActivity.java
@@ -2,8 +2,10 @@ package com.biborton.kachabazarandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +22,22 @@ public class MainActivity extends AppCompatActivity {
     private void initViews(){
         wvHomePage = (WebView) findViewById(R.id.homePage);
         wvHomePage.loadUrl(URLMap.homePage);
+
         WebSettings webSettings = wvHomePage.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        wvHomePage.setWebViewClient(new CustomWebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && wvHomePage.canGoBack()) {
+            wvHomePage.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class CustomWebViewClient extends WebViewClient{
+
     }
 }
