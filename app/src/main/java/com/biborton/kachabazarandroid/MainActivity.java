a0491e34362cdf83af9feffdbb19863c96cf4300
Ahsan <ahsantarique@ymail.com>
2018-04-01 19:20:03 +0600

    started homepage

--- app/src/main/java/com/biborton/kachabazarandroid/MainActivity.java
+++ app/src/main/java/com/biborton/kachabazarandroid/MainActivity.java
@@ -2,12 +2,25 @@ package com.biborton.kachabazarandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView wvHomePage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews(){
+        wvHomePage = (WebView) findViewById(R.id.homePage);
+        wvHomePage.loadUrl(URLMap.homePage);
+        WebSettings webSettings = wvHomePage.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
---
+++ app/src/main/java/com/biborton/kachabazarandroid/URLMap.java
@@ -0,0 +1,6 @@
+package com.biborton.kachabazarandroid;
+
+public class URLMap {
+
+    public static String homePage = "https://kachabazar.herokuapp.com/";
+}
